6886f4a7b070a506fa29ca799088f54f3a8b802a
Endi Glavinić <endi@doen-international.eu>
2018-03-08 15:37:06 +0100

    Add project files.

---
+++ SystecCalc/frmCalc.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace SystecCalc
+{
+    partial class frmCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.tbOperations = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn1.Location = new System.Drawing.Point(13, 197);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(56, 43);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbResult.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.tbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbResult.Location = new System.Drawing.Point(12, 45);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(304, 37);
+            this.tbResult.TabIndex = 3;
+            this.tbResult.Text = "0";
+            this.tbResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn2
+            // 
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn2.Location = new System.Drawing.Point(75, 197);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(56, 43);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn3.Location = new System.Drawing.Point(137, 197);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(56, 43);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn4.Location = new System.Drawing.Point(13, 148);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(56, 43);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn5.Location = new System.Drawing.Point(75, 148);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(56, 43);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn6.Location = new System.Drawing.Point(137, 148);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(56, 43);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn7.Location = new System.Drawing.Point(13, 99);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(56, 43);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn8.Location = new System.Drawing.Point(75, 99);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(56, 43);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn9.Location = new System.Drawing.Point(137, 99);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(56, 43);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn0.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn0.Location = new System.Drawing.Point(13, 246);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(118, 43);
+            this.btn0.TabIndex = 12;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnDot
+            // 
+            this.btnDot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDot.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDot.Location = new System.Drawing.Point(137, 246);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(56, 43);
+            this.btnDot.TabIndex = 13;
+            this.btnDot.Text = ".";
+            this.btnDot.UseVisualStyleBackColor = false;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiv.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDiv.Location = new System.Drawing.Point(199, 99);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(56, 43);
+            this.btnDiv.TabIndex = 14;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = false;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTimes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTimes.Location = new System.Drawing.Point(199, 148);
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.Size = new System.Drawing.Size(56, 43);
+            this.btnTimes.TabIndex = 15;
+            this.btnTimes.Text = "*";
+            this.btnTimes.UseVisualStyleBackColor = false;
+            this.btnTimes.Click += new System.EventHandler(this.btnTimes_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMinus.Location = new System.Drawing.Point(199, 197);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(56, 43);
+            this.btnMinus.TabIndex = 16;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnPlus.Location = new System.Drawing.Point(199, 246);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(56, 43);
+            this.btnPlus.TabIndex = 17;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnC.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnC.Location = new System.Drawing.Point(261, 99);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(56, 92);
+            this.btnC.TabIndex = 18;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEqual.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEqual.Location = new System.Drawing.Point(261, 197);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(56, 92);
+            this.btnEqual.TabIndex = 19;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = false;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // tbOperations
+            // 
+            this.tbOperations.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOperations.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbOperations.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.tbOperations.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbOperations.Location = new System.Drawing.Point(12, 26);
+            this.tbOperations.Name = "tbOperations";
+            this.tbOperations.Size = new System.Drawing.Size(304, 22);
+            this.tbOperations.TabIndex = 20;
+            this.tbOperations.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // frmCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 298);
+            this.Controls.Add(this.tbOperations);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btn1);
+            this.KeyPreview = true;
+            this.Name = "frmCalc";
+            this.Text = "Danieli Systec Calculator";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmCalc_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.TextBox tbOperations;
+    }
+}
+
---
+++ SystecCalc/frmCalc.cs
@@ -0,0 +1,308 @@
+﻿//This program is created for testing purposes, however, fell free to change any parts of it
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystecCalc
+{
+    public partial class frmCalc : Form
+    {
+        //Allows resetting display
+        private static bool allowResetRes = false;
+        public static bool AllowResetRes { get => allowResetRes; set => allowResetRes = value; }
+        //Stack stores both variables and operators
+        Stack calcStack = new Stack();
+
+        public frmCalc() => InitializeComponent();
+     
+
+        //function to move content of calc display and operators to stack
+        private void stackIt(string varToStack, string opToStack)
+        {
+            string tempFirstNum, tempSecondNum, tempOperator;
+            //Checks for beginning of calculation and do nothing for "=" sign
+            if (calcStack.Count == 0 && opToStack != "=")
+            {
+                calcStack.Push(varToStack);
+                calcStack.Push(opToStack);
+            } else if (calcStack.Count == 0 && opToStack == "=")
+            {
+                //do nothing here
+            } else if (calcStack.Count == 2) //when there is one number and one operation on stack
+            {
+                //If second operation is one of menitoned we first have to calculate last input number with just enterend number
+                //and then put it on stack with just entered operation
+                if (opToStack == "+" || opToStack == "-" ||opToStack =="=")
+                {
+
+                    tempOperator = calcStack.Pop().ToString();
+                    tempSecondNum = varToStack;
+                    tempFirstNum = calcStack.Pop().ToString();
+                    //reclycle of tempFirstNum
+                    tempFirstNum = calculate(tempFirstNum, tempOperator, tempSecondNum);
+                    tbResult.Text = tempFirstNum;
+                    //If the stack here is empty and we dont want to store anything if "=" is pressed, just display it
+                    if (opToStack != "=")
+                    {
+                        calcStack.Push(tempFirstNum);
+                        calcStack.Push(opToStack);
+                    }
+                } else
+                //In case of "*" or "/" we store it to stack
+                {
+                    calcStack.Push(varToStack);
+                    calcStack.Push(opToStack);
+                }
+
+            } else
+            {   //when stack is nor 0 or 2 elements we assume we have more elements
+                //and we first have to multiply and divide...
+                tempOperator = calcStack.Pop().ToString();
+                tempSecondNum = calculate(calcStack.Pop().ToString(), tempOperator, varToStack);
+                //...then we have to preform other operation from stack
+                tempOperator = calcStack.Pop().ToString();
+                tempFirstNum = calculate(calcStack.Pop().ToString(), tempOperator, tempSecondNum);
+
+                //If the stack here is empty and we dont want to store anything if "=" is pressed, just display it
+                if (opToStack != "=")
+                {
+                    calcStack.Push(tempFirstNum);
+                    calcStack.Push(opToStack);
+                }
+                tbResult.Text = tempFirstNum;
+            }
+        }
+
+        private string calculate(string first, string op, string second)
+        {
+            double calcResult = 0;
+            switch (op)
+            {
+                case "*":
+                    calcResult = float.Parse(first) * float.Parse(second);
+                    break;
+                case "/":
+                    calcResult = 0;
+                    if (float.Parse(second) != 0)
+                    {
+                        calcResult = float.Parse(first) / float.Parse(second);
+                    }
+                    break;
+                case "+":
+                    calcResult = float.Parse(first) + float.Parse(second);
+                    break;
+                case "-":
+                    calcResult = float.Parse(first) - float.Parse(second);
+                    break;
+            }
+            return calcResult.ToString();
+        }
+
+        private void updateResult(string numAdd)
+        {
+            float value;
+            //if number or dot is entered, just add it to display
+            if (float.TryParse(numAdd, out value) || numAdd == ".")
+            {
+                if ((tbResult.Text == "0" && numAdd != ".") || AllowResetRes == true) tbResult.Text = "";
+                AllowResetRes = false;
+                tbResult.Text += numAdd;
+            }
+            else
+            {
+                if (numAdd == "+" || numAdd == "-" || numAdd == "*" || numAdd == "/" || numAdd == "=")
+                {
+                    stackIt(tbResult.Text, numAdd);
+                    tbOperations.Text = numAdd;
+                    AllowResetRes = true;
+                } else if (numAdd == "C")
+                {   //reset tbOperations value
+                    tbOperations.Text = "";
+                    //Reset tbResult value
+                    tbResult.Text = "0";
+                    //Empty calcStack
+                    while (calcStack.Count > 0)
+                    {
+                        calcStack.Pop();
+                    }
+                }
+
+            }
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnTimes_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            updateResult(((System.Windows.Forms.ButtonBase)sender).Text);
+        }
+
+        private void frmCalc_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D1:
+                    e.Handled = true;
+                    btn1.PerformClick();
+                    break;
+                case Keys.D2:
+                    e.Handled = true;
+                    btn2.PerformClick();
+                    break;
+                case Keys.D3:
+                    e.Handled = true;
+                    btn3.PerformClick();
+                    break;
+                case Keys.D4:
+                    e.Handled = true;
+                    btn4.PerformClick();
+                    break;
+                case Keys.D5:
+                    e.Handled = true;
+                    btn5.PerformClick();
+                    break;
+                case Keys.D6:
+                    e.Handled = true;
+                    btn6.PerformClick();
+                    break;
+                case Keys.D7:
+                    e.Handled = true;
+                    if (e.Shift != true)
+                    {
+                        btn7.PerformClick();
+                    } else
+                    {
+                        btnDiv.PerformClick();
+                    }
+                    break;
+                case Keys.D8:
+                    e.Handled = true;
+                    btn8.PerformClick();
+                    break;
+                case Keys.D9:
+                    e.Handled = true;
+                    btn9.PerformClick();
+                    break;
+                case Keys.D0:
+                    e.Handled = true;
+                    btn0.PerformClick();
+                    break;
+                case Keys.Enter:
+                    e.Handled = true;
+                    btnEqual.PerformClick();
+                    break;
+                case Keys.Oemplus:
+                    e.Handled = true;
+                    if (e.Shift != true)
+                    {
+                        btnPlus.PerformClick();
+                    } else btnTimes.PerformClick();
+                    break;
+                case Keys.OemMinus:
+                    e.Handled = true;
+                    btnMinus.PerformClick();
+                    break;
+
+                case Keys.Delete:
+                    e.Handled = true;
+                    btnC.PerformClick();
+                    break;
+                case Keys.OemPeriod:
+                    e.Handled = true;
+                    btnDot.PerformClick();
+                    break;
+                case Keys.Oemcomma:
+                    e.Handled = true;
+                    btnDot.PerformClick();
+                    break;
+            }
+        }
+    }
+}
